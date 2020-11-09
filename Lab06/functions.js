8f23729771ba6dc1e4509a9e729ad63fd6ceedf7
Hussien Elgabry <hussien.elgabry@live.com>
2020-11-08 19:42:52 -0600

    Lab 06

---
+++ Lab06/functions.js
@@ -0,0 +1,45 @@
+// Exercise 1
+String.prototype.filter = function(...str){
+    return this.split(' ').filter((word)=>str.indexOf(word)<0).join(" ");
+};
+
+// Exercise 2
+Array.prototype.bubbleSort = function(){
+    let len = this.length;
+    for(let i = 0; i < len; ++i)
+        for(let j = 0; j < len; ++j)
+            if(this[j] > this[j+1])
+            {
+                let temp = this[j];
+                this[j] = this[j+1];
+                this[j+1] = temp;
+            }
+    return this;
+};
+
+//Exercise 3
+function Person(name,age)
+{
+    this.name=name;
+    this.age =age;
+}
+Person.prototype.describe=function()
+{
+    return this.name + ", " + this.age + " years old.";
+}
+
+function Teacher(name,age)
+{
+    this.name=name;
+    this.age=age;
+}
+
+Teacher.prototype.__proto__ = Person.prototype;
+
+Teacher.prototype.teach=function(subject)
+{
+    return this.name + " teaches " + subject;
+}
+
+let teacher1 = new Teacher("Siko",24);
+console.log(teacher1.teach("WAP"));
